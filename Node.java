63a33c43c7c8c7ae48cb1245fce24c9114b74438
LauraHernandez22 <102169050+LauraHernandez22@users.noreply.github.com>
2023-11-27 23:25:30 -0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Node here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Node
+{
+    	String name; //campo de datos
+	Node previous;//campo antecesor
+        Node next;   //campo enlace
+
+}
---
+++ UsaListaDoble.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class UsaListaDoble here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UsaListaDoble
+{
+   public static void main (String[]args){
+       ListaDoble lista = new ListaDoble();
+       lista.insertaPrimerNodo("R");
+       lista.imprimir();
+       lista.insertaPrimerNodo("F");
+       lista.imprimir();
+       System.out.println(lista);
+       lista.insertaAntesPrimerNodo("H");
+       System.out.println(lista);
+       lista.insertaAlFinal("Z");
+       System.out.println(lista);
+       lista.insertaEntreNodos("T", "R");
+       System.out.println(lista);
+       lista.insertaAntesPrimerNodo("K");
+       System.out.println(lista);
+       lista.insertaAlFinal("Ñ");
+       System.out.println(lista);
+       lista.borrarPrimerNodo();
+       System.out.println(lista);
+       lista.borrarUltimoNodo();
+       System.out.println(lista);
+       lista.borrarCualquierNodo("R");
+       System.out.println(lista);
+       /* ----------------------------------------------------- */
+        lista.intercambiarNodos("H","R");
+        System.out.println(lista);
+        lista.buscarNodo("H");
+        System.out.println(lista);
+        /* ----------------------------------------------------- */
+   }
+}
